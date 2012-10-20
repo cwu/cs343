056ae38036cca69e73328bbbf4c3c5939502574b
Chris Wu <me@chriswu.me>
2012-10-20 04:41:28 -0400

    part 1 of q1

--- a3/q1buffer.h
+++ a3/q1buffer.h
@@ -1,6 +1,191 @@
+#ifndef Q1_BUFFER_H
+#define Q1_BUFFER_H
+
+#include <uC++.h>
+#include <assert.h>
+#include <list>
+
+using namespace std;
+
+#ifdef BUSY
 template<typename T> class BoundedBuffer {
+  private:
+    const unsigned int size;
+    list<T> buffer;
+    uOwnerLock lock;
+    uCondLock fullCond;
+    uCondLock emptyCond;
+
+    bool isFull();
+    bool isEmpty();
   public:
     BoundedBuffer( const unsigned int size = 10 );
     void insert( T elem );
     T remove();
 };
+
+template<typename T>
+BoundedBuffer<T>::BoundedBuffer( const unsigned int size) : size(size) {}
+
+template<typename T>
+bool BoundedBuffer<T>::isFull() {
+    return buffer.size() == size;
+}
+
+template<typename T>
+bool BoundedBuffer<T>::isEmpty() {
+    return buffer.size() == 0;
+}
+
+template<typename T>
+void BoundedBuffer<T>::insert(T elem) {
+    lock.acquire();
+    while (isFull()) {
+        fullCond.wait(lock);
+    }
+
+    assert(!isFull());
+    buffer.push_back(elem);
+    emptyCond.signal();
+    lock.release();
+}
+
+template<typename T>
+T BoundedBuffer<T>::remove() {
+    lock.acquire();
+    while (isEmpty()) {
+        emptyCond.wait(lock);
+    }
+
+    assert(!isEmpty());
+    T temp = buffer.front();
+    buffer.pop_front();
+    fullCond.signal();
+    lock.release();
+    return temp;
+}
+
+#endif // BUSY
+
+#ifdef NOBUSY
+template<typename T> class BoundedBuffer {
+  private:
+    const unsigned int size;
+    list<T> buffer;
+    uOwnerLock lock;
+    uCondLock fullCond;
+    uCondLock emptyCond;
+    uCondLock bargingCond;
+    bool signalFlag;
+
+    bool isFull();
+    bool isEmpty();
+  public:
+    BoundedBuffer( const unsigned int size = 10 );
+    void insert( T elem );
+    T remove();
+};
+
+template<typename T>
+BoundedBuffer<T>::BoundedBuffer( const unsigned int size)
+    : size(size), signalFlag(false) {
+}
+
+template<typename T>
+bool BoundedBuffer<T>::isFull() {
+    return buffer.size() == size;
+}
+
+template<typename T>
+bool BoundedBuffer<T>::isEmpty() {
+    return buffer.size() == 0;
+}
+
+template<typename T>
+void BoundedBuffer<T>::insert(T elem) {
+    lock.acquire();
+
+    static int signalEmpty = 0;
+
+    // check if we are barging
+    if (signalFlag) {
+        cout << "  > barging into insert : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        bargingCond.wait(lock);
+    }
+
+    if (isFull()) {
+        if (!bargingCond.empty()) {
+            cout << " - signal barging (insert wait) : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+            bargingCond.signal();
+        }
+        cout << " > sleep on full : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        fullCond.wait(lock);
+        cout << " < wake from full : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+    }
+
+    assert(!isFull());
+    buffer.push_back(elem);
+    cout << "insert : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+
+    signalFlag = !emptyCond.empty();
+    if (!emptyCond.empty()) {
+        assert(!isEmpty());
+        cout << " - signal not empty : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        signalFlag = true;
+        signalEmpty++;
+        cout << "   - signal empty : " << signalEmpty << " times." << endl;
+        emptyCond.signal();
+    } else if (!bargingCond.empty()) {
+        cout << " - signal barging (insert) : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        bargingCond.signal();
+    }
+
+    lock.release();
+}
+
+template<typename T>
+T BoundedBuffer<T>::remove() {
+    lock.acquire();
+
+    static int waitEmpty = 0;
+
+    if (signalFlag) {
+        cout << "  > barging into remove : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        bargingCond.wait(lock);
+    }
+
+    if (isEmpty()) {
+        if (!bargingCond.empty()) {
+            cout << " - signal barging (remove wait) : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+            bargingCond.signal();
+        }
+        cout << " > sleep on empty : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        waitEmpty++;
+        cout << "   - wait empty : " << waitEmpty << " times." << endl;
+        emptyCond.wait(lock);
+        cout << " < wake from empty : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+    }
+    cout << "remove : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+
+    assert(!isEmpty());
+    T temp = buffer.front();
+    buffer.pop_front();
+
+    signalFlag = !fullCond.empty();
+    if (!fullCond.empty()) {
+        cout << " - signal not full : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        fullCond.signal();
+    } else if (!bargingCond.empty()) {
+        cout << " - signal barging (remove) : " << buffer.size() << " [" << lock.owner() << "] " << endl;
+        bargingCond.signal();
+    } else {
+        cout << "nothing on the queue : " << fullCond.empty() << " " << bargingCond.empty() << " " << signalFlag << endl;
+    }
+
+    lock.release();
+    return temp;
+}
+#endif
+
+
+#endif // Q1_BUFFER_H
---
+++ a3/q1main.cc
@@ -0,0 +1,152 @@
+#include <uC++.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "q1buffer.h"
+#include "MPRNG.h"
+
+using namespace std;
+#include <cstdlib>
+
+const int DEFAULT_CONS = 5;
+const int DEFAULT_PRODS = 3;
+const int DEFAULT_PRODUCE = 10;
+const int DEFAULT_BUFFER_SIZE = 10;
+
+static MPRNG randomGen;
+
+bool convert(int &val, char *buffer ) {    // convert C string to integer
+    std::stringstream ss( buffer );         // connect stream and buffer
+    ss >> dec >> val;                       // convert integer from buffer
+    return ! ss.fail() &&                   // conversion successful ?
+                                            // characters after conversion all blank ?
+            string( buffer ).find_first_not_of( " ", ss.tellg() ) == string::npos;
+} // convert
+
+/**
+ * Prints the usage and exits the program.
+ *
+ * argv - the program argument values
+ */
+static void usage(char **argv) {
+    cerr << "usage: " << argv[0]
+         << " [ Cons [ Prods [ Produce [ Buffer Size [ Delays ]]]]]" << endl
+         << " defaults:" << endl
+         << "   Cons - " << DEFAULT_CONS << endl
+         << "   Prods - " << DEFAULT_PRODS << endl
+         << "   Produce - " << DEFAULT_PRODUCE << endl
+         << "   Buffer Size - " << DEFAULT_BUFFER_SIZE << endl
+         << "   Delays - Prods + Cons" << endl;
+    exit(EXIT_FAILURE);
+}
+
+_Task Producer {
+    BoundedBuffer<int> &buffer;
+    const int Produce;
+    const int Delay;
+
+    void main() {
+        for (int produce = 1; produce <= Produce; produce++) {
+            yield(randomGen(Delay));
+            buffer.insert(produce);
+        }
+    }
+  public:
+    Producer( BoundedBuffer<int> &buffer, const int Produce, const int Delay )
+        : buffer(buffer), Produce(Produce), Delay(Delay) {}
+};
+
+_Task Consumer {
+    BoundedBuffer<int> &buffer;
+    const int Delay;
+    const int Sentinel;
+    int &sum;
+
+    void main() {
+        yield(randomGen(Delay));
+        int value = buffer.remove();
+        while (value != Sentinel) {
+            // accumulate sum only after we know it is not the Sentinel value
+            sum += value;
+
+            // get a new value
+            yield(randomGen(Delay));
+            value = buffer.remove();
+        }
+    }
+  public:
+    Consumer( BoundedBuffer<int> &buffer, const int Delay, const int Sentinel,
+              int &sum )
+        : buffer(buffer), Delay(Delay), Sentinel(Sentinel), sum(sum) {}
+};
+
+void uMain::main() {
+    if (argc > 6) {
+        usage(argv);
+    }
+
+    // Parse command line
+    int cons = DEFAULT_CONS;
+    if (argc >= 2 && (!convert(cons, argv[1]) || cons <= 0)) {
+        usage(argv);
+    }
+
+    int prods = DEFAULT_PRODS;
+    if (argc >= 3 && (!convert(prods, argv[2]) || prods <= 0)) {
+        usage(argv);
+    }
+
+    int produce = DEFAULT_PRODUCE;
+    if (argc >= 4 && (!convert(produce, argv[3]) || produce <= 0)) {
+        usage(argv);
+    }
+
+    int bufferSize = DEFAULT_BUFFER_SIZE;
+    if (argc >= 5 && (!convert(bufferSize, argv[4]) || bufferSize <= 0)) {
+        usage(argv);
+    }
+
+    int delays = cons + prods;
+    if (argc >= 6 && (!convert(delays, argv[5]) || delays <= 0)) {
+        usage(argv);
+    }
+
+    // Setup the tasks
+    BoundedBuffer<int> buffer(bufferSize);
+
+    Producer *producers[prods];
+    for (int prod = 0; prod < prods; prod++) {
+        producers[prod] = new Producer(buffer, produce, delays);
+    }
+
+    Consumer *consumers[cons];
+    int partialSums[cons];
+    int sentinel = -1;
+    for (int con = 0; con < cons; con++) {
+        partialSums[con] = 0;
+        consumers[con] = new Consumer(buffer, delays, sentinel,
+                                      partialSums[con]);
+    }
+
+    // Wait for producers to finish
+    for (int prod = 0; prod < prods; prod++) {
+        delete producers[prod];
+    }
+
+    // insert sentinel values into the buffer and wait for consumers to finish
+    for (int con = 0; con < cons; con++) {
+        buffer.insert(sentinel);
+    }
+    for (int con = 0; con < cons; con++) {
+        delete consumers[con];
+    }
+
+    // sum the partial sums
+    int sum = 0;
+    for (int con = 0; con < cons; con++) {
+        sum += partialSums[con];
+    }
+    cout << "total: " << sum << endl;
+}
